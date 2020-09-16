bd79e12166bf198840c44d0ccec9112ef78f821f
esteban7590 <esteban7590@hotmail.com>
2020-09-16 10:48:23 -0500

    Added form and handler

--- src/components/MemeGenerator.js
+++ src/components/MemeGenerator.js
@@ -9,6 +9,7 @@ class MemeGenerator extends Component {
       image: "http://i.imgflip.com/1bij.jpg",
       allMemeImgs: [],
     };
+    this.handleChange = this.handleChange.bind(this);
   }
 
   componentDidMount() {
@@ -16,13 +17,45 @@ class MemeGenerator extends Component {
       .then((response) => response.json())
       .then((response) => {
         const { memes } = response.data;
-        console.log("memes:", memes[0]);
         this.setState({ allMemeImgs: memes });
       });
   }
 
+  handleChange(event) {
+    const { name, value } = event.target;
+    this.setState({ [name]: value });
+    console.log(this.state.topText);
+  }
+
   render() {
-    return <h2>Generator section</h2>;
+    return (
+      <div>
+        <form>
+          <input
+            type="text"
+            name="topText"
+            placeholder="Top Text"
+            value={this.state.topText}
+            onChange={this.handleChange}
+          />
+          <br />
+          <input
+            type="text"
+            name="bottomText"
+            placeholder="Bottom Text"
+            value={this.state.bottomText}
+            onChange={this.handleChange}
+          />
+          <br />
+          <button>Generate</button>
+        </form>
+        <div>
+          <img src={this.state.image} alt="" />
+          <h2>{this.state.topText}</h2>
+          <h2>{this.state.bottomText}</h2>
+        </div>
+      </div>
+    );
   }
 }
 
