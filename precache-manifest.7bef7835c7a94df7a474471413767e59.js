06445be4f35dd19b35ed5b3804316f0d28946b2b
esteban7590 <esteban7590@hotmail.com>
2020-09-18 09:36:03 -0500

    Updates

---
+++ precache-manifest.7bef7835c7a94df7a474471413767e59.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27461de25a37c52640f4ce28473ce978",
+    "url": "/react-meme-generator/index.html"
+  },
+  {
+    "revision": "a0c73dcb524ef5f41a2c",
+    "url": "/react-meme-generator/static/css/main.38166fbc.chunk.css"
+  },
+  {
+    "revision": "bfbc91ec685afa280554",
+    "url": "/react-meme-generator/static/js/2.c0dab115.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-meme-generator/static/js/2.c0dab115.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a0c73dcb524ef5f41a2c",
+    "url": "/react-meme-generator/static/js/main.0580528a.chunk.js"
+  },
+  {
+    "revision": "a252d0789912461d7403",
+    "url": "/react-meme-generator/static/js/runtime-main.ebf92e4e.js"
+  }
+]);
