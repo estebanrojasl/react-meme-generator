697ee3632cca2339942ed6edc914dc35e6dc5f7e
esteban7590 <esteban7590@hotmail.com>
2020-10-05 12:14:29 -0500

    Changed to functional components - hooks

--- src/components/MemeGenerator.js
+++ src/components/MemeGenerator.js
@@ -1,69 +1,126 @@
-import React, { Component } from "react";
+import React from "react";
+import { useEffect } from "react";
+import { useState } from "react";
 
-class MemeGenerator extends Component {
-  constructor() {
-    super();
-    this.state = {
-      topText: "",
-      bottomText: "",
-      image: "http://i.imgflip.com/1bij.jpg",
-      allMemeImgs: [],
-    };
-    this.handleChange = this.handleChange.bind(this);
-    this.generate = this.generate.bind(this);
-  }
+const MemeGenerator = () => {
+  const [text, setText] = useState({ topText: "", bottomText: "" });
+  const [image, setImage] = useState("http://i.imgflip.com/1bij.jpg");
+  const [allMemes, setAllMemes] = useState([]);
 
-  componentDidMount() {
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setText({ ...text, [name]: value });
+  };
+
+  const generate = (event) => {
+    event.preventDefault();
+    const position = Math.floor(Math.random() * allMemes.length);
+    const { url } = allMemes[position];
+    setImage(url);
+  };
+
+  useEffect(() => {
     fetch("https://api.imgflip.com/get_memes")
       .then((response) => response.json())
       .then((response) => {
-        const { memes } = response.data;
-        this.setState({ allMemeImgs: memes });
+        setAllMemes(response.data.memes);
       });
-  }
+  }, []);
 
-  handleChange(event) {
-    const { name, value } = event.target;
-    this.setState({ [name]: value });
-  }
+  return (
+    <main>
+      <form>
+        <input
+          type="text"
+          name="topText"
+          placeholder="Top Text"
+          value={text.topText}
+          onChange={handleChange}
+        />
+        <br />
+        <input
+          type="text"
+          name="bottomText"
+          placeholder="Bottom Text"
+          value={text.bottomText}
+          onChange={handleChange}
+        />
+        <br />
+        <button onClick={generate}>Generate</button>
+      </form>
+      <div className="meme">
+        <img src={image} alt="" />
+        <h2>{text.topText}</h2>
+        <h2>{text.bottomText}</h2>
+      </div>
+    </main>
+  );
+};
 
-  generate(event) {
-    event.preventDefault();
-    const position = Math.floor(Math.random() * this.state.allMemeImgs.length);
-    const { url } = this.state.allMemeImgs[position];
-    this.setState({ image: url });
-  }
+// class MemeGenerator extends Component {
+//   constructor() {
+//     super();
+//     this.state = {
+//       topText: "",
+//       bottomText: "",
+//       image: "http://i.imgflip.com/1bij.jpg",
+//       allMemeImgs: [],
+//     };
+//     this.handleChange = this.handleChange.bind(this);
+//     this.generate = this.generate.bind(this);
+//   }
 
-  render() {
-    return (
-      <main>
-        <form>
-          <input
-            type="text"
-            name="topText"
-            placeholder="Top Text"
-            value={this.state.topText}
-            onChange={this.handleChange}
-          />
-          <br />
-          <input
-            type="text"
-            name="bottomText"
-            placeholder="Bottom Text"
-            value={this.state.bottomText}
-            onChange={this.handleChange}
-          />
-          <br />
-          <button onClick={this.generate}>Generate</button>
-        </form>
-        <div className="meme">
-          <img src={this.state.image} alt="" />
-          <h2>{this.state.topText}</h2>
-          <h2>{this.state.bottomText}</h2>
-        </div>
-      </main>
-    );
-  }
-}
+//   componentDidMount() {
+//     fetch("https://api.imgflip.com/get_memes")
+//       .then((response) => response.json())
+//       .then((response) => {
+//         const { memes } = response.data;
+//         this.setState({ allMemeImgs: memes });
+//       });
+//   }
+
+//   handleChange(event) {
+//     const { name, value } = event.target;
+//     this.setState({ [name]: value });
+//   }
+
+//   generate(event) {
+//     event.preventDefault();
+//     const position = Math.floor(Math.random() * this.state.allMemeImgs.length);
+//     const { url } = this.state.allMemeImgs[position];
+//     this.setState({ image: url });
+//   }
+
+//   render() {
+//     return (
+//       <main>
+//         <form>
+//           <input
+//             type="text"
+//             name="topText"
+//             placeholder="Top Text"
+//             value={this.state.topText}
+//             onChange={this.handleChange}
+//           />
+//           <br />
+//           <input
+//             type="text"
+//             name="bottomText"
+//             placeholder="Bottom Text"
+//             value={this.state.bottomText}
+//             onChange={this.handleChange}
+//           />
+//           <br />
+//           <button onClick={this.generate}>Generate</button>
+//         </form>
+//         <div className="meme">
+//           <img src={this.state.image} alt="" />
+//           <h2>{this.state.topText}</h2>
+//           <h2>{this.state.bottomText}</h2>
+//         </div>
+//       </main>
+//     );
+//   }
+// }
 
 export default MemeGenerator;
